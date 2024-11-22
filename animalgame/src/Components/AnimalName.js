6faae32bb4353acbf8514a6e8fd086c15b9a0659
Musny Mubarak <119862882+musnymubarak@users.noreply.github.com>
2024-11-22 14:43:43 +0000

    AnimalName component & Random name show logic created

---
+++ animalgame/src/Components/AnimalName.js
@@ -0,0 +1,8 @@
+export default function AnimalName({name}){
+    return(
+        <div className="animal-name">
+            <h2>Animal Name</h2>
+            <h3>{name}</h3>
+        </div>
+    )
+}
